6b397e8aefd2a0bee642b8116d0869168ca749a1
Fernando Cesar <81422214+fernandocesarrussie@users.noreply.github.com>
2021-10-01 09:50:11 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://harrypottervnw.netlify.app",
+//     siteUrl: "https://www.yourdomain.tld",
     title: "Harry Potter",
   },
   plugins: ["gatsby-plugin-styled-components"],
